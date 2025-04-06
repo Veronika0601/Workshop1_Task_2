70047c33c4ded11b7d9c6ccd904055309a176c22
Veronika <veronikatodorova@abv.bg>
2025-04-06 12:07:43 +0300

    Configure chrome driver

--- TestProject1/Table.cs
+++ TestProject1/Table.cs
@@ -2,6 +2,7 @@ using System.Collections.ObjectModel;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.BiDi.Modules.Network;
 
 namespace TestProject2
 {
@@ -13,8 +14,22 @@ namespace TestProject2
         [SetUp]
         public void SetUp()
         {
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");
+            options.AddArgument("no-sandbox");
+            options.AddArgument("disable-dev-shm-usage");
+            options.AddArgument("disable-gpu");
+
+
+            string userDataDir= Path.Combine(Path.GetTempPath(),Path.GetRandomFileName());
+
+            Directory.CreateDirectory(userDataDir);
+            options.AddArgument($"--user-data-dir={userDataDir}");
+
+
             // Create object of ChromeDriver
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
 
             // Add implicit wait
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
